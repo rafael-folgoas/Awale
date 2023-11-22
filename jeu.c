d7086535ec89361a24ca6ad966379c4877cc43d3
Rafael Folgoas <rafael.folgoas@insa-lyon.fr>
2023-11-22 12:01:15 +0100

    dernier commit

--- jeu.c
+++ jeu.c
@@ -28,6 +28,7 @@ bool jouerCoup(Jeu *jeu,int coup,Joueur* joueur){
     jeu->plateau[coup] = 0;
     // Distribuer les billes aux cases suivantes
     for (int i = 1; i <= billes; i++) {
+        
         jeu->plateau[(coup + i) % 12]++;
     }
 
@@ -57,7 +58,9 @@ bool jouerCoup(Jeu *jeu,int coup,Joueur* joueur){
 Jeu* initJeu(Joueur *j1,Joueur *j2){
     Jeu *jeu =(Jeu*)malloc(sizeof(Jeu));
     for (int i=0;i<12;i++){
+      
         jeu->plateau[i]=4;
+        
     }
     
     jeu->joueur1=j1;
@@ -83,6 +86,38 @@ bool legaliteCoup(Jeu *jeu,int coup){
     }
     if(jeu->plateau[coup]==0){
         return false;
+    }
+    //coup du joueur 1
+    if (jeu->tour==0){
+        bool vide=true;
+        int case1=6;
+        while(vide&&case1<12){
+            if(jeu->plateau[case1]!=0){
+                vide=false;
+            }
+            case1++;
+        }
+        if(vide){
+            if( coup+jeu->plateau[coup]<=5){
+                return false;
+            }
+        }
+    }
+    //coup du joueur 2
+    if (jeu->tour==1){
+        bool vide=true;
+        int case2=0;
+        while(vide&&(case2<6)){
+            if(jeu->plateau[case2]!=0){
+                vide=false;
+            }
+            case2++;
+        }
+        if(vide){
+            if(coup+jeu->plateau[coup]<=11){
+                return false;
+            }
+        }
     }
     return true;
 
@@ -117,4 +152,66 @@ void afficherTableau(Jeu *jeu) {
     }
     printf("\n---------------------------\n");
 }
+
+
+int finPartie(Jeu *jeu){
+    if(jeu->scoreJ1>25){
+        return 0;
+    }else if(jeu->scoreJ2>25){
+        return 1;
+    }
+
+    //coup du joueur 1
+    if (jeu->tour==0){
+        bool vide=true;
+        int case1=6;
+        while(vide&&case1<12){
+            if(jeu->plateau[case1]!=0){
+                vide=false;
+            }
+            case1++;
+        }
+        if(vide){
+            for (int i=0;i<6;i++){
+                if (jeu->plateau[i]+i>=6){
+                    jeu->scoreJ1+=jeu->billesRestantes;
+                    if (scoreJ2>scoreJ1){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }
+            }
+            
+            return 0;
+        }
+    }
+    //coup du joueur 2
+    if (jeu->tour==1){
+        bool vide=true;
+        int case2=0;
+        while(vide&&(case2<6)){
+            if(jeu->plateau[case2]!=0){
+                vide=false;
+            }
+            case2++;
+        }
+        if(vide){
+            for (int i=6;i<12;i++){
+                if (jeu->plateau[i]+i>11){
+                    jeu->scoreJ2+=jeu->billesRestantes;
+                    if (scoreJ2>scoreJ1){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+
+    return -1;
+}
+
 
